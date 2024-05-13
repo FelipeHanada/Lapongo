b88b924935188f312ae7b3a2bbeb85e3505f55a8
Felipe Hanada <felipe.eizohanada@gmail.com>
2024-05-13 17:54:06 -0300

    feat add player to game scene

---
+++ src/game_objects/game_scene/player.py
@@ -0,0 +1,13 @@
+from pgframework import *
+
+
+class GameScenePlayer(AbstractGameObject):
+    _sprite_file_path = 'src/assets/sprites/characters/calango/calango_idle.png'
+    _sprite_sheet = SpriteSheetGrid(_sprite_file_path, 2, 2, 32, 32)
+    _flipbook = _sprite_sheet.get_flip_book_from_pack(4, True)
+    _flipbook_timed = FlipBookTimed.get_from_flip_book(_flipbook, 0.4)
+
+    def __init__(self, *args, **kwargs):
+        AbstractGameObject.__init__(self, *args, **kwargs, rect=pygame.Rect(112, 180, 32, 32))
+
+        self.add_component(Sprite2DAnimated(self, self._flipbook_timed))
--- src/scenes/game_scene.py
+++ src/scenes/game_scene.py
@@ -1,8 +1,10 @@
 from pgframework import *
 from src.game_objects.game_scene.background import GameSceneBackground
+from src.game_objects.game_scene.player import GameScenePlayer
 
 class GameScene(AbstractScene):
     def __init__(self, game: Game):
         AbstractScene.__init__(self, game, (255, 255, 255))
 
         self.add_scene_game_object(GameSceneBackground, priority=0)
+        self.add_scene_game_object(GameScenePlayer, priority=1)
